c82633722f7a40ebde415f2d5db167680332a84d
Sam <samsmith0817@gmail.com>
2020-10-27 19:38:31 -0400

    Adding menu starters

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public bool escKey;
+    public bool paused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        escKey = Input.GetKeyDown(KeyCode.Escape);
+
+        if (escKey)
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+        {
+            OpenPause();
+        }
+        else
+        {
+            ClosePause();
+        }
+    }
+
+    // Pause Menu functions
+    void OpenPause()
+    {
+        pauseMenu.gameObject.SetActive(true);
+        paused = true;
+    }
+
+    public void ClosePause()
+    {
+        pauseMenu.gameObject.SetActive(false);
+        paused = false;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    // buttons
+    public Button newGame;
+    public Button loadGame;
+    public Button options;
+    public Button instructions;
+    public Button credits;
+    public Button exit;
+
+    // pop ups
+    public Button creditsPopup;
+    public Button instructionsPopup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        creditsPopup.gameObject.SetActive(false);
+        instructionsPopup.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Disable all menu buttons (useful when displaying messages)
+    void DisableButtons()
+    {
+        newGame.interactable = false;
+        loadGame.interactable = false;
+        options.interactable = false;
+        instructions.interactable = false;
+        credits.interactable = false;
+        exit.interactable = false;
+    }
+    // Enables all buttons
+    void EnableButtons()
+    {
+        newGame.interactable = true;
+        loadGame.interactable = true;
+        options.interactable = true;
+        instructions.interactable = true;
+        credits.interactable = true;
+        exit.interactable = true;
+    }
+
+    // Functions for handling menu loading
+    public void NewGame()
+    {
+        SceneManager.LoadScene("GameWorld");
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene("ContinueMenu");
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exiting Game in .exe");
+    }
+
+
+    // Functions for handling UI popups
+    public void CreditsButton()
+    {
+        creditsPopup.gameObject.SetActive(true);
+        DisableButtons();
+    }
+
+    public void CreditsPopup()
+    {
+        creditsPopup.gameObject.SetActive(false);
+        EnableButtons();
+    }
+
+    public void InstructionsButton()
+    {
+        instructionsPopup.gameObject.SetActive(true);
+        DisableButtons();
+    }
+
+    public void InstructionsPopup()
+    {
+        instructionsPopup.gameObject.SetActive(false);
+        EnableButtons();
+    }
+}
