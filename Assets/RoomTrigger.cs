2d0a1ea4d29ad6e8cb33f40af9cd47daf545e235
VindettaP <vincenttran@umass.edu>
2020-12-04 21:44:23 -0500

    Added some stuff to level

---
+++ Assets/RoomTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTrigger : MonoBehaviour
+{
+    public BoxCollider trigger;
+    public GameObject wall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trigger = gameObject.GetComponent<BoxCollider>();
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.name == "PlayerBody"){
+            float step = 200 * Time.deltaTime;
+            Vector3 activated = new Vector3(wall.transform.position.x, wall.transform.position.y + 6.64f, wall.transform.position.z);
+            wall.transform.position = Vector3.MoveTowards(wall.transform.position, activated, step); 
+            Destroy(this);
+        }
+    }
+}
